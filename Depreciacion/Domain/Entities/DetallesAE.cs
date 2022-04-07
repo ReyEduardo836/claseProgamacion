e1e101102aee9d9531f532df6ea2ba7b48d5dd8d
ReyEduardo836 <reyeduardohalsallquintero8@gmail.com>
2022-04-07 07:51:53 -0600

    Agregando la clase DetallesAE

--- Depreciacion/Domain/Entities/DetallesAE.cs
+++ Depreciacion/Domain/Entities/DetallesAE.cs
@@ -6,12 +6,7 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    class DetallesAE // SIZE 25
+    class DetallesAE
     {
-        public int Id { get; set; } // 4
-        public int IdEmpleado { get; set; } //4
-        public int IdActivo { get; set; } //4
-        public long FechaInicio { get; set; } // 8
-        public bool IsActivo { get; set; }// 1
     }
 }
